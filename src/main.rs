1f34194eefb1060b3125c439a738bdc666094ce6
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2025-03-06 01:23:38 +0530

    added ratatui example couter app

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,106 @@
-use clap::Parser;
-use indicatif::{ProgressBar, ProgressStyle};
-use std::thread;
-use std::time::Duration;
+use std::io;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Crate a timer for specified value in mins
-    #[arg(short, long, default_value_t = 10)]
-    create: u64,
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
 
-    /// Give timer a name
-    #[arg(short, long, default_value_t = String::from("Pomodoro"))]
-    name: String,
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+
+    app_result
 }
 
-fn main() {
-    let welcome_banner = include_str!("../ascii.txt");
-    println!("{}", welcome_banner);
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
 
-    let args = Args::parse();
-    let min = args.create;
-    println!("Name: {}", args.name);
-    println!("Duration: {} mins", min);
+impl App {
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        loop {
+            if self.exit {
+                break;
+            }
 
-    let total_seconds = min * 60;
-
-    // prepare progress bar
-    let bar = ProgressBar::new(total_seconds);
-    let default_style = ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/green}")
-        .unwrap()
-        .progress_chars("#>-");
-    bar.set_style(default_style);
-
-    let mut seconds = Duration::from_secs(total_seconds);
-    let wait = Duration::from_secs(1);
-
-    loop {
-        thread::sleep(wait);
-        if seconds.is_zero() {
-            break;
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
         }
-
-        if seconds == Duration::from_secs(total_seconds / 2) {
-            println!("50% of time is completed.");
-        }
-
-        seconds = seconds.saturating_sub(Duration::from_secs(1));
-        bar.inc(1);
+        Ok(())
     }
 
-    bar.finish();
-    println!("Time is completed.");
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Counter App Tutorial ".bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
 }
