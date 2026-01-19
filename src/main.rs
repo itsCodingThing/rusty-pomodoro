29147fef7bde0bcd9f103a1ec9f0f1c486517d66
bhanu pratap singh <bhanuparitapsingh09@gmai.com>
2026-01-20 04:59:25 +0530

    update the commands and working on features

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,97 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use indicatif::{ProgressBar, ProgressStyle};
 use std::thread;
 use std::time::Duration;
+mod storage;
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Crate a timer for specified value in mins
-    #[arg(short, long, default_value_t = 10)]
-    create: u64,
+#[command(name = "Pomo", version = "1.0.0", about = "a pomodoro counter", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    /// Give timer a name
-    #[arg(short, long, default_value_t = String::from("Pomodoro"))]
-    name: String,
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    #[command(name = "add", about = "add a timer to storage")]
+    Add {
+        /// Crate a timer for specified value in mins
+        /// minimum value should be 5 min
+        #[arg(short, long, default_value_t = 10)]
+        duration: u64,
+
+        /// Give timer a name
+        #[arg(short, long)]
+        name: String,
+    },
+
+    #[command(arg_required_else_help = true)]
+    #[command(name = "create", about = "create a quick timer")]
+    Create {
+        /// Give timer a name
+        #[arg(short, long)]
+        name: String,
+
+        /// Crate a timer for specified value in mins
+        /// minimum value should be 5 min
+        #[arg(short, long)]
+        duration: u64,
+    },
+
+    #[command(arg_required_else_help = true)]
+    #[command(name = "run", about = "run a stored timer by name")]
+    Run {
+        /// Give timer a name
+        #[arg(short, long)]
+        name: String,
+    },
 }
 
 fn main() {
     let welcome_banner = include_str!("../ascii.txt");
     println!("{}", welcome_banner);
 
-    let args = Args::parse();
-    let min = args.create;
-    println!("Name: {}", args.name);
-    println!("Duration: {} mins", min);
+    let cli = Cli::parse();
+    storage::create_storage();
 
-    let total_seconds = min * 60;
+    match cli.command {
+        Commands::Create { duration, name } => {
+            let secs = duration * 60;
 
-    // prepare progress bar
-    let bar = ProgressBar::new(total_seconds);
+            println!("name: {name}");
+            setup_timer(secs);
+            println!("Timer is complete")
+        }
+        Commands::Add { name, duration } => {
+            println!("name: {name:}\nduration: {duration}");
+        }
+        Commands::Run { name } => {
+            println!("name: {name}\n");
+        }
+    }
+}
+
+fn setup_timer(duration: u64) {
+    let bar = ProgressBar::new(duration);
     let default_style = ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/green}")
         .unwrap()
-        .progress_chars("#>-");
+        .progress_chars("#>*");
     bar.set_style(default_style);
 
-    let mut seconds = Duration::from_secs(total_seconds);
+    let mut seconds = Duration::from_secs(duration);
     let wait = Duration::from_secs(1);
 
     loop {
         thread::sleep(wait);
-        if seconds.is_zero() {
-            break;
-        }
-
-        if seconds == Duration::from_secs(total_seconds / 2) {
-            println!("50% of time is completed.");
-        }
 
         seconds = seconds.saturating_sub(Duration::from_secs(1));
         bar.inc(1);
+
+        if seconds.is_zero() {
+            break;
+        }
     }
 
     bar.finish();
-    println!("Time is completed.");
 }
